268245edf82e33e7effd0b3bc073965871ec39be
Rodrigo Gomez Palacio <rogomezpalacio@gmail.com>
2018-11-02 16:26:12 -0500

    logic

--- routes/index.js
+++ routes/index.js
@@ -4,32 +4,74 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 var router = express.Router();
 
-function getResponse(counter, message){
+function getResponse(counter, message, lang){
+  // questions 2-4
+  let questions = [
+    [
+      "What size is your family? (example: 4)",
+      "¿De que tamaño es su familia? (ejemplo: 4)"
+    ],
+    [
+      "Is your household income less than ",
+      "¿Es el ingreso de su hogar menos de "
+    ],
+    ["You qualify for ", "Usted es elegible para recibir"]
+  ]
+
+  // INITIAL MESSAGES
   switch(counter){
     case 0:
-      return "Hello! You may qualify for the Supplemental Nutrition Assistance Program (SNAP). Would you like to continue? (reply yes or no)";
+      return "Hello! You may qualify for the Supplemental Nutrition Assistance Program (SNAP). To continue in English, send 1 \nHola! Su hogar pude ser eligible para SNAP. Para continuar en Español, envie 2";
       break;
     case 1:
-      if(message.toLowerCase() == 'yes' || message.toLowerCase() == 'y'){
-        return "Great! Let's get started."
+      if(message === '1' || message === '2') return setLang(message);
+      break;
+    case 2:
+      if(message === "yes" || message === "si"){
+        return "Please call ___";
+      }
+      if(message === "no"){
+        return questions[0][lang];
       }
       break;
+    case 3:
+      // get value depending on family size
+
+      return questions[1][lang]+//value
+      break;
+    case 4:
+      if(message === "yes" || message === "si"){
+        return questions[2][lang];
+      }
     default:
       return "Something went wrong";
   }
   return '$INVALID$'; // invalid response
 }
 
+function setLang(value){
+  if(value === 1) return 0; // english
+  if(value === 2) return 1; // spanish
+}
+
 /* GET home page. */
 router.post('/sms', function(req, res, next) {
-  let message = req.body.Body;
+  let message = req.body.Body.toLowerCase();
   console.log("message:", message);
   const smsCount = req.session.counter || 0;
 
-  var response = getResponse(smsCount, message);
+  var response = getResponse(smsCount, message, req.session.language);
+
+  // set language, send question 1
+  if(smsCount == 1){
+    req.session.language = response;
+    response = response === 0 ? "Do you get state benefits now? (yes/no)":"/¿Usted recibe beneficios del estado actualmente? (si/no)";
+  }
+
   console.log("response:", response);
+  // send other questions
   if(response !== '$INVALID$') req.session.counter = smsCount + 1;
-  else console.error('Invalid message');
+  else response = "Sorry, I didn't get that. Please provide a valid response.";
 
   const twiml = new MessagingResponse();
   twiml.message(response);
