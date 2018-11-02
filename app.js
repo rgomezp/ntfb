33309467ce46dd7766a60e4b8d2ecd4be89f12b6
Rodrigo Gomez Palacio <rogomezpalacio@gmail.com>
2018-11-02 14:14:16 -0500

    make post

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.use(session({secret: process.env.SECRET}));
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,7 +18,6 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
+const express = require('express');
+const http = require('http');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/sms', function(req, res, next) {
+  const smsCount = req.session.counter || 0;
+
+  var message = 'Hello, thanks for the new message.';
+
+  if(smsCount > 0) {
+    message = 'Hello, thanks for message number ' + (smsCount + 1);
+  }
+
+  req.session.counter = smsCount + 1;
+
+  const twiml = new MessagingResponse();
+  twiml.message(message);
+
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
 });
 
 module.exports = router;
