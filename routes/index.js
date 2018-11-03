8062a3b9da0fd96fd5a3888294b696d571a1f86c
Rodrigo Gomez Palacio <rogomezpalacio@gmail.com>
2018-11-03 13:12:22 -0500

    everything works

--- routes/index.js
+++ routes/index.js
@@ -4,107 +4,148 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 var router = express.Router();
 
-function getResponse(counter, message, lang){
-  // questions 2-4
-  let questions = [
-    [
-      "What size is your family? (example: 4)",
-      "¿De que tamaño es su familia? (ejemplo: 4)"
-    ],
-    [
-      "Is your household income less than ",
-      "¿Es el ingreso de su hogar menos de "
-    ],
-    ["You qualify for ", "Usted es elegible para recibir beneficios. Por favor marque 214-269-0906 para completar su aplicacion."]
-  ]
-
-  switch(counter){
-    // W E L C O M E, P L E A S E  S E L E C T  L A N G U A G E
-    case 0:
-      return "Hello! You may qualify for the Supplemental Nutrition Assistance Program (SNAP). To continue in English, send 1 \n---------\n Hola! Su hogar pude ser eligible para SNAP. Para continuar en Español, envie 2";
-      break;
-    // L A N G U A G E  S E L E C T O R
-    case 1:
-      if(message === '1' || message === '2') return setLang(message);
-      else return "$INVALID$";
-      break;
-    // D O   Y O U   A L R E A D Y  R E C E I V E  B E N E F I T S?
-    case 2:
-      if(message === "no"){
-        return questions[0][lang];
-      }
-      if(message === "yes"){
-        return "$FINISHED$";
-      }
-      break;
-    // D O   Y O U   M A K E   L E S S   T H A N
-    case 3:
-      let famIncomes = {
-        1:"$1659",
-        2:"$2233",
-        3:"$2808",
-        4:"$3383",
-        5:"$3958",
-      }
-      let value = famIncomes[Number(message)];
-      return questions[1][lang]+value+'?';
-      break;
-    // Y O U  Q U A L I F Y , S E N D  M E S S A G E  W I T H  P H O N E  N U M B E R
-    case 4:
-      if(message === "yes" || message === "si"){
-        return questions[2][lang];
-      }
-      if(message === "no"){
-        return "$UNQUALIFIED$";
-      }
-      break;
-    default:
-      return "$FINISHED$";
+class Node{
+  constructor(id, double, logic, left, right){
+    this.id = id;
+    this.double = double;
+    this.logic = logic;
+    this.left = left;
+    this.right = right;
   }
-  return '$INVALID$'; // invalid response
+}
+
+const tree = new Node(0, false, {}, null, new Node(1, false, {1:'', 2:''}, null,
+  new Node(3, true, {'yes':'left', 'no': 'right'},
+    new Node(2, false, {}, null, null),
+    new Node(4, false, {}, null,
+      new Node(5, false,{
+        1:'',
+        2:'',
+        3:'',
+        4:'',
+        5:'',
+        6:'',
+        7:'',
+        8:'',
+        9:''
+      }, null,
+        new Node(7, true, {'no':'left', 'yes':'right'},
+          new Node(6, false, {}, null, null),
+          new Node(8, false, {}, null, null)
+        ))
+    )
+  )
+));
+
+var responses = {
+  0:['Hello! You may qualify for the Supplemental Nutrition Assistance Program (SNAP). To continue in English, type 1\n------------\nHola! Su hogar puede ser elegible para el Supplemental Nutrition Assistance Program (SNAP). Para continuar en Español, envie 2',
+     'Hello! You may qualify for the Supplemental Nutrition Assistance Program (SNAP). To continue in English, type 1\n------------\nHola! Su hogar puede ser elegible para el Supplemental Nutrition Assistance Program (SNAP). Para continuar en Español, envie 2'],
+  1:['Do you currently get state benefits? (yes/no)',
+     'Usted recibe beneficios del estado actualmente?'],
+  2:['Great! Please call 214-269-0906 to complete your application. Have a good day!',
+     'Exelente! Favor de marcar 214-269-0906 para completar su aplicacion. Que tenga un buen dia!'],
+  4:['What size is your family? (example: 4)',
+     'De que tamaño es su familia? (ejemplo: 4)'],
+  5:['Is your household income less than ',
+     'Es el ingreso de su hogar menos de '],
+  6:["While you don't qulify for automatic benefits, your family may still be eligible for the SNAP program. Please call 214-269-0906 to complete your application. Have a good day!",
+     'Mientras no califica para beneficios automaticos, es posible que su familia todavia pueda calificar para el programa SNAP. Favor de marcar 214-269-0906 para completar su aplicacion. Que tenga un buen dia!'],
+  8:['Congratulations! You qualify for the SNAP program. Please call 214-269-0906 to complete your application. Have a good day!',
+     'Felicidades! Usted califica para el SNAP program. Favor de marcar 214-269-0906 para completar su aplicacion. Que tenga un buen dia!']
+}
+
+const familyIncome = {
+  1:"$1659",
+  2:"$2233",
+  3:"$2808",
+  4:"$3383",
+  5:"$3958"
 }
 
 function setLang(value){
-  if(value === '1') return 0; // english
-  else if(value === '2') return 1; // spanish
+  if(Number(value) === 1) return 0; // english
+  else if(Number(value) === 2) return 1; // spanish
   else return -1; // not valid
+}
+
+function findNode(node, id){
+  if(node.id === id) return node;
+
+  if(node.id < id){
+    // go right
+    return findNode(node.right, id);
+  }
+  if(node.id > id){
+    return findNode(node.left, id);
+  }
 }
 
 /* GET home page. */
 router.post('/sms', function(req, res, next) {
-  let message = req.body.Body.toLowerCase().split(' ')[0];
-  console.log("message:", message);
-  const smsCount = req.session.counter || 0;
+  try{
+    let invalid = false;
+    const twiml = new MessagingResponse();
 
-  var response = getResponse(smsCount, message, req.session.language);
-  console.log('initial resp:', response);
+    // init msg code
+    let msgCode = req.session.msgCode || 0;
 
-  // OVERRIDE RESPONSE ONCE LANGUAGE IS SET
-  if(response !== '$INVALID$' && smsCount == 1){
-    req.session.language = response;
-    response = response === 0 ? "Do you get state benefits now? (yes/no)":"¿Usted recibe beneficios del estado actualmente? (si/no)";
+    // find node, logic
+    let currentNode = findNode(tree, msgCode);
+    let logic = currentNode.logic;
+
+    // get msg
+    let message = req.body.Body.toLowerCase().split(' ')[0];
+    let response;
+
+    // get lang
+    let lang = req.session.lang || 0;
+    // set lang
+    if(msgCode === 1) lang = setLang(message);
+
+    if(lang !== -1) req.session.lang = lang;
+    else invalid = true;
+
+    // check if msg is valid
+    if(Object.keys(currentNode.logic).indexOf(message) === -1) invalid = true;
+
+    if(invalid && currentNode.id > 0) response = "Please type a valid response";
+    else{
+      response;
+      var next;
+      // get next node
+      if(currentNode.double){
+        dir = logic[message];
+        currentNode = currentNode[dir];
+        response = responses[currentNode.id][lang];
+      }else{
+        if(currentNode.id===5){
+          // get direction based on income and family size
+          let income = familyIncome[Number(message)];
+          response = responses[currentNode.id][lang] + income + '?';
+        }else response = responses[currentNode.id][lang];
+      }
+      next = currentNode.right;
+      if(next == null){
+        next = tree;
+      }
+
+      if(next.id >= 0) req.session.msgCode = next.id;
+    }
+
+    console.log("response:", response);
+
+    twiml.message(response);
+
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end(twiml.toString());
+  }catch(err){
+    console.log(err);
+    twiml.message(err);
+
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end(twiml.toString());
   }
 
-  // send other questions
-  if(response !== '$INVALID$') req.session.counter = smsCount + 1;
-  else response = "Sorry, I didn't get that. Please provide a valid response.";
-
-  if(response == '$FINISHED$'){
-    response = "Please call 214-269-0906 to complete your application.";
-    req.session.counter = 0;
-  }
-
-  if(response == '$UNQUALIFIED$'){
-    response = "While you dont qualify for automatic benefits, your family may still be eligibile for the SNAP program. Please call 214-269-0906 to complete your application.";
-    req.session.counter = 0;
-  }
-  console.log("response:", response);
-
-  const twiml = new MessagingResponse();
-  twiml.message(response);
-
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
 });
 
 module.exports = router;
